b434de7ee0d883a79e5d5482272a3c0b44286920
Ujjwal9329 <ujjwalgujral8@gmail.com>
2025-03-13 16:59:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
 
+//your code here!
+
+let list_var = document.getElementById('infi-list');
+list_var.addEventListener('scroll',function (e) {
+    while((list_var.scrollTop + list_var.clientHeight) >= list_var.scrollHeight - 5) {
+        let innerList1 = document.createElement('li');
+        let innerList2 = document.createElement('li');
+
+        // getting last no.
+        let last_list_number = list_var.lastElementChild;
+        let no_lln = parseInt(last_list_number.textContent);
+
+        // let ol_var = document.getElementById('infi-list');
+        
+        // adding continue numbers
+        innerList1.textContent=`${no_lln+1}`;
+        innerList2.textContent=`${no_lln+2}`;
+
+        list_var.appendChild(innerList1);
+        list_var.appendChild(innerList2);
+    }
+});
